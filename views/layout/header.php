0ae0284a7479a8b88d226951e2175504c097c941
BadWiX505 <houssamnaouali04@gmail.com>
2025-04-23 11:52:41 +0100

    first commit

---
+++ views/layout/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestion des Événements</title>
+    <link rel="stylesheet" href="../../public/css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="/eventsphp/views/events/create_event.php">📅 Créer un Événement</a>
+            <a href="/eventsphp/views/events/list_events.php">📋 Liste des Événements</a>
+            <a href="/eventsphp/views/participants/register_participant.php">🧑 Inscrire un Participant</a>
+            <a href="/eventsphp/views/inscriptions/list_inscriptions.php">✅ Inscriptions</a>
+        </nav>
+    </header>
+    <main>
